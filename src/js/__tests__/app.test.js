5d884fc0ada0e8d2fcbd39a50c5128e3ec612875
kxrxll <kovalyovkirill@gmail.com>
2021-08-10 11:43:16 +0300

    solved

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,33 @@
-import sum from '../basic';
+import orderByProps from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('straight test', () => {
+  const result = orderByProps({
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  }, ['name', 'level']);
+  const expected = [
+    { name: 'мечник' },
+    { level: 2 },
+    { attack: 80 },
+    { defence: 40 },
+    { health: 10 }];
+  expect(result).toEqual(expected);
+});
 
-  expect(result).toBe(6);
+test('alphabetical test', () => {
+  const result = orderByProps({
+    a: 1,
+    d: 4,
+    b: 2,
+    c: 3,
+  }, []);
+  const expected = [
+    { a: 1 },
+    { b: 2 },
+    { c: 3 },
+    { d: 4 }];
+  expect(result).toEqual(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
+const orderByProps = (obj, arr) => {
+  const resultSortedByInput = [];
+  const resultSortedAlphabetical = [];
+  arr.forEach((item) => {
+    resultSortedByInput.push({ [item]: obj[item] });
+  });
+  for (const key in obj) {
+    if (!arr.includes(key)) {
+      resultSortedAlphabetical.push({
+        [key]: obj[key],
+      });
+    }
+  }
+  resultSortedAlphabetical.sort((a, b) => {
+    if (Object.keys(a)[0] < Object.keys(b)[0]) { return -1; }
+    return 1;
+  });
+  return resultSortedByInput.concat(resultSortedAlphabetical);
+};
 
+export default orderByProps;
